a1c3b6c40ff9ff01436633151df6ed11ce4d96ac
devsany <sunnyrajbodhgaya19@gmail.com>
2024-08-13 19:51:54 +0530

    hook 2 example complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Hook1 from "./component/useState/Hook1";
+// import Hook1 from "./component/useState/Hook1";
+import Hook2 from "./component/useState/Hook2";
 
 function App() {
-  return <div className="App">{/* <Hook1 /> */}</div>;
+  return (
+    <div className="App">
+      {/* <Hook1 /> */}
+      <Hook2 />
+    </div>
+  );
 }
 
 export default App;
